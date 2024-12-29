1b463e0ff3c99e0b26a8a4a432d8c189f5ba4330
Nani2139 <129682715+Nani2139@users.noreply.github.com>
2024-12-29 23:48:50 +0530

    Add API views using ViewSets for Form Builder application

--- forms/views.py
+++ forms/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Form, Question, Option, Response, Answer
+from .serializers import FormSerializer, QuestionSerializer, OptionSerializer, ResponseSerializer, AnswerSerializer
 
-# Create your views here.
+class FormViewSet(viewsets.ModelViewSet):
+    queryset = Form.objects.all()
+    serializer_class = FormSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class OptionViewSet(viewsets.ModelViewSet):
+    queryset = Option.objects.all()
+    serializer_class = OptionSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ResponseViewSet(viewsets.ModelViewSet):
+    queryset = Response.objects.all()
+    serializer_class = ResponseSerializer
+    permission_classes = [permissions.AllowAny]  # Allow anonymous submissions
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
